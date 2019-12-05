96673343c93b2be182a278503040911df701a605
elliottaisaac <49501453+elliottaisaac@users.noreply.github.com>
2019-12-05 13:33:37 -0600

    elevator 3

---
+++ elevator3.cpp
@@ -0,0 +1,66 @@
+#include <cstdio>
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+using namespace std;
+
+
+class Elevator
+{
+    public:
+
+    vector<int> FloorHistory = {0};
+    
+    
+    void InputFloor(int currentF, vector<int>FloorHistory){
+        vector<int> newDestinations = {};
+        cout << "\nEnter destination floor: \n" << endl;
+        int *goToFloor = new int;
+        cin >> *goToFloor;
+        newDestinations.insert(newDestinations.end(), *goToFloor);
+        int nextFloor = -1;
+        while(nextFloor != 0){
+            cout << "\nEnter next destination floor or enter '0' to start elevator \n" << endl;
+            cin >> nextFloor;
+            newDestinations.insert(newDestinations.end(), nextFloor);
+        }
+        ChangeFloors(currentF, FloorHistory, newDestinations);
+    }
+
+    void ChangeFloors(int current, vector<int> FloorHistory, vector<int> newDestinations){
+        for(int e = 0; e < newDestinations.size() - 1; e++){
+            int floorDiff = newDestinations[e] - current;
+            int s = 1;
+                if(floorDiff < 0){
+                    s = -1;
+                }
+            
+            for(int f = FloorHistory.back(); f != newDestinations[e] && f <= 100 && f >= 0; f = f + s){
+                if(f == 101){
+                    newDestinations[e] = 100;
+                    f = 100;
+                    puts("You have reached the top floor.");
+                }
+                if(f <= 1){
+                    f = 1;
+                    puts("You are on the ground floor.");
+                }
+                FloorHistory.insert(FloorHistory.end(), f + s);
+                cout << "\nYou are on floor: " << FloorHistory.back() << endl;
+            }
+            current = newDestinations[e];
+        }
+        InputFloor(current, FloorHistory);
+    }
+};
+
+
+int main()
+{
+    Elevator E;
+    E.InputFloor(E.FloorHistory.back(), E.FloorHistory);
+    return 0;
+}
